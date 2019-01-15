f811342869146aa227f7d570e9c2d9553a8bc173
Amit Shukla <amit090shukla@gmail.com>
2019-01-14 22:20:05 -0800

    Code Refactor for Todo's App

--- src/Completed.jsx
+++ src/Completed.jsx
@@ -1,33 +1,38 @@
 import React from "react";
 import * as _ from "lodash";
 import Check from "@material-ui/icons/CheckCircle";
+import styled from "styled-components";
+
+// Styled Components
+const CompletedWrapper = styled.div`
+  margin-left: "20px";
+  margin-bottom: "15px";
+  padding: "20px";
+  background: "rgb(99, 177, 99)";
+  color: "#fff";
+  border-radius: "8px";
+`;
+const HeadingStyle = styled.span`
+  font-weight: 600;
+`;
+
+const CompletedTodos = styled.div`
+  background: rgb(122, 195, 122);
+  margin: 10px 0;
+  padding: 3px;
+`;
+//------------------------------------------------
 export default ({ todos }) => {
   const completed = _.filter(todos, ["complete", true]);
   return (
-    <div
-      style={{
-        marginLeft: "20px",
-        marginBottom: "15px",
-        padding: "20px",
-        background: "rgb(99, 177, 99)",
-        color: "#fff",
-        borderRadius: "8px"
-      }}
-    >
-      <span style={{ fontWeight: "600" }}>Completed Tasks</span>
+    <CompletedWrapper>
+      <HeadingStyle>Completed Tasks</HeadingStyle>
       {completed.map((val, key) => (
-        <div
-          key={key}
-          style={{
-            background: "rgb(122, 195, 122)",
-            margin: "10px 0",
-            padding: "3px"
-          }}
-        >
+        <CompletedTodos key={key}>
           <Check fontSize="small" />
           <span style={{ marginLeft: "15px" }}>{val.text}</span>
-        </div>
+        </CompletedTodos>
       ))}
-    </div>
+    </CompletedWrapper>
   );
 };
--- src/Pending.jsx
+++ src/Pending.jsx
@@ -1,35 +1,39 @@
 import React from "react";
 import * as _ from "lodash";
 import RemoveCircle from "@material-ui/icons/RemoveCircle";
+import styled from "styled-components";
 
+// Styled Components
+const PendingWrapper = styled.div`
+  margin-left: 20px;
+  margin-bottom: 15px;
+  padding: 20px;
+  background: #ff7575;
+  color: #fff;
+  border-radius: 8px;
+`;
+const HeadingStyle = styled.span`
+  font-weight: 600;
+`;
+
+const PendingTodos = styled.div`
+  background: rgb(255, 135, 135);
+  margin: 10px 0;
+  padding: 3px;
+`;
+//------------------------------------------------------
 export default ({ todos }) => {
   const pending = _.filter(todos, ["complete", false]);
 
   return (
-    <div
-      style={{
-        marginLeft: "20px",
-        marginBottom: "15px",
-        padding: "20px",
-        background: "#ff7575",
-        color: "#fff",
-        borderRadius: "8px"
-      }}
-    >
-      <span style={{ fontWeight: "600" }}>Pending Tasks</span>
+    <PendingWrapper>
+      <HeadingStyle style={{ fontWeight: "600" }}>Pending Tasks</HeadingStyle>
       {pending.map((val, key) => (
-        <div
-          key={key}
-          style={{
-            background: "rgb(255, 135, 135)",
-            margin: "10px 0",
-            padding: "3px"
-          }}
-        >
+        <PendingTodos key={key}>
           <RemoveCircle fontSize="small" />
           <span style={{ marginLeft: "15px" }}>{val.text}</span>
-        </div>
+        </PendingTodos>
       ))}
-    </div>
+    </PendingWrapper>
   );
 };
